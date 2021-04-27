93eb93a0491b9d4da14434d1d9dd3d2b4e973f5a
Priyadharshni05 <69076776+Priyadharshni05@users.noreply.github.com>
2021-04-27 17:06:29 +0530

    Update Activity1.c

--- src/Activity1.c
+++ src/Activity1.c
@@ -1,1 +1,32 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void port()
+{
+    DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD0);
+    DDRD&=~(1<<PD2);
+
+    PORTD|=(1<<PD0);
+    PORTD|=(1<<PD2);
+    return 0;
+}
+
+void led()
+{
+    while(1)
+    {
+        if((!(PIND & (1<<PD0)) && (!(PIND & (1<<PD2)))))
+        {
+            PORTB |=(1<<PB0);
+            _delay_ms(2000);
+        }
+        else
+        {
+            PORTB &=~(1<<PB0);
+            _delay_ms(2000);
+        }
+    }
+    return 0;
+}
 
