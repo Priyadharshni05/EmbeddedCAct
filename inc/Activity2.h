607d05ebf0ecaf7f20efd8847bfcea57b239ba03
Priyadharshni05 <69076776+Priyadharshni05@users.noreply.github.com>
2021-04-30 14:04:01 +0530

    Update Activity2.h

--- inc/Activity2.h
+++ inc/Activity2.h
@@ -1,8 +1,18 @@
 #ifndef ACTIVITY2_H_INCLUDED
 #define ACTIVITY2_H_INCLUDED
 
-
+/**
+ * @brief ADC Initialisation
+ * @note Initialise the ADC 
+ * @return void
+ */
 void ADC_Init(void);
+
+/**
+ * @brief Read the ADC Channel
+ * 
+ * @return uint16_t =>ADC Value=>Voltage converted to digital value
+ */
 uint16_t ReadADC(uint8_t);
 
 
