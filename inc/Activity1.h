1ae51b005ab27522bafa8c810429f69ff815a0b8
Priyadharshni05 <69076776+Priyadharshni05@users.noreply.github.com>
2021-04-30 14:03:28 +0530

    Update Activity1.h

--- inc/Activity1.h
+++ inc/Activity1.h
@@ -1,16 +1,16 @@
 #ifndef ACTIVITY1_H_INCLUDED
 #define ACTIVITY1_H_INCLUDED
 
-/** \brief port initialisation
- *
- * \return void
+/** @brief port initialisation
+ * @note define the switch and LED ports
+ * @return void
  *
  */
 void port_init(void);
 
-/** \brief Controlling the LED (switching on/off the LED)
- *
- * \return void
+/** @brief Controlling the LED (switching on/off the LED)
+ *@note ON/OFF LED
+ * @return int=>Status of the LED
  *
  */
 
