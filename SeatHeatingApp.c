c10850d669ea296c213ad348b7407f3c827561e7
Priyadharshni05 <69076776+Priyadharshni05@users.noreply.github.com>
2021-04-30 12:02:20 +0530

    Update SeatHeatingApp.c

--- SeatHeatingApp.c
+++ SeatHeatingApp.c
@@ -1,9 +1,33 @@
+/*
+ */
+
 #include <avr/io.h>
 #include <util/delay.h>
-#include <Activity1.h>
+#include "Activity1.h"
+#include "Activity2.h"
+#include "Activity3.h"
+#include "Activity4.h"
+
 int main(void)
 {
-    port_init();
-    led ();
+
+    while(1)
+    {
+        port_init();
+        int status=0;
+        status=led_init();
+        if(status==1)
+        {
+            ADC_Init();
+            Timer_Init();
+            USART_init(103);
+            uint16_t temp = 0;
+			temp = ReadADC(0);
+			_delay_ms(200);
+			pwm_waveform(temp);	/*Generation of PWM according to the temperature value*/
+			_delay_ms(200);
+			USARTWritemsg(temp);
+        }
+    }
     return 0;
 }
