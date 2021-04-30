2f4a2e222e5257af385fb0447a1b27a8e32202d4
Priyadharshni05 <69076776+Priyadharshni05@users.noreply.github.com>
2021-04-30 14:01:34 +0530

    Update Activity1.c

--- src/Activity1.c
+++ src/Activity1.c
@@ -5,11 +5,11 @@
 void port_init()
 {
     DDRB|=(1<<PB0); //Port PB0->LED->Output
-    DDRD&=~(1<<PD0);
-    DDRD&=~(1<<PD2);
+    DDRD&=~(1<<PD0);//Port PD0->Switch->Seated or not
+    DDRD&=~(1<<PD2);//Port PD2->Switch->Heater on or not
 
-    PORTD|=(1<<PD0);
-    PORTD|=(1<<PD2);
+    PORTD|=(1<<PD0);//Writing switch as 1(5V)
+    PORTD|=(1<<PD2);//Writing switch as 1(5V)
     return 0;
 }
 
@@ -17,13 +17,13 @@ int led_init()
 {
         if((!(PIND & (1<<PD0)) && (!(PIND & (1<<PD2)))))
         {
-            PORTB |=(1<<PB0);
+            PORTB |=(1<<PB0);//LED On
             _delay_ms(2000);
             return 1;
         }
         else
         {
-            PORTB &=~(1<<PB0);
+            PORTB &=~(1<<PB0);//LED Off
             _delay_ms(2000);
             return 0;
         }
