95ecb3471a554e4282d0ef6770d94cfe0334c4e9
Priyadharshni05 <69076776+Priyadharshni05@users.noreply.github.com>
2021-04-30 14:05:03 +0530

    Update Activity4.h

--- inc/Activity4.h
+++ inc/Activity4.h
@@ -3,8 +3,24 @@
 
 #define F_CPU 16000000UL
 
+/**
+ * @brief Initializing the USART
+ * 
+ * @return void
+ */
 void USART_init(uint16_t);
+
+/**
+ * @brief USART Reads the msg from UDR
+ * 
+ * @return char => msg
+ */
 char USARTReadmsg();
+
+/**
+ * @brief USART writes the msg to UDR
+ * 
+ */
 void USARTWritemsg(uint16_t);
 
 
