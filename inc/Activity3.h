c062a49b43303dfcc43291b39e2053c87afe8a67
Priyadharshni05 <69076776+Priyadharshni05@users.noreply.github.com>
2021-04-30 14:04:31 +0530

    Update Activity3.h

--- inc/Activity3.h
+++ inc/Activity3.h
@@ -1,7 +1,15 @@
 #ifndef ACTIVITY3_H_INCLUDED
 #define ACTIVITY3_H_INCLUDED
 
-
+/**
+ * @brief Initializing the Timer
+ * 
+ */
 void Timer_Init(void);
+
+/**
+ * @brief Generate the waveform 
+ * 
+ */
 void pwm_waveform(uint16_t);
 #endif // ACTIVITY3_H_INCLUDED
